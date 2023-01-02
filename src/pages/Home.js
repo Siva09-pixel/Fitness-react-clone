0b8a0e0356ad7db16755e2b154fdfb1dfef57da9
Siva Pratap P <111338491+Siva09-pixel@users.noreply.github.com>
2023-01-02 17:36:00 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React,{useEffect,useState} from 'react';
+import {Box} from '@mui/material'
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Excercises'
+
+const Home = () => {
+  const [exercises,setExercises]=useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+  return (
+    <div>
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises}
+       bodyPart={bodyPart} 
+       setBodyPart={setBodyPart}  />
+      <Exercises
+      setExercises={setExercises}
+        bodyPart={bodyPart}
+        exercises={exercises} />
+    </div>
+  )
+}
+
+export default Home
